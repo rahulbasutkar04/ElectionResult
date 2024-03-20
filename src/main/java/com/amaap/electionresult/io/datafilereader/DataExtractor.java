3087f5e1eeb612a41d33791bb56653af449ee02a
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-20 11:56:26 +0530

    Added Cstome Exception for Invalid File Format And test cases for it.

--- src/main/java/com/amaap/electionresult/io/datafilereader/DataExtractor.java
+++ src/main/java/com/amaap/electionresult/io/datafilereader/DataExtractor.java
@@ -1,9 +1,18 @@
 package com.amaap.electionresult.io.datafilereader;
-
+import com.amaap.electionresult.customeexceptions.*;
 public class DataExtractor {
 
-    public boolean readFile() {
 
-        return true;
+    public boolean readFile(String path) throws IllegalFileFormatException {
+        if (!path.toLowerCase().endsWith(".txt")) {
+            throw new IllegalFileFormatException("Invalid file format. Please provide a .txt file.");
+        }
+
+        // Proceed with reading the file
+        // Your file reading logic here
+
+        return true; // Placeholder return statement
     }
+
+
 }
--- src/test/java/DataExtractorTest.java
+++ src/test/java/DataExtractorTest.java
@@ -1,23 +1,38 @@
 
+import com.amaap.electionresult.customeexceptions.IllegalFileFormatException;
 import com.amaap.electionresult.io.datafilereader.DataExtractor;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DataExtractorTest {
 
     @Test
-    void shouldAbleToReadFileFromTheUser()
-    {
+    void shouldAbleToReadFileFromTheUser() throws IllegalFileFormatException {
         //Arrange
-        DataExtractor dataExctractor=new DataExtractor();
-
-        //Assert
-        boolean isFileRaid=dataExctractor.readFile();
+        DataExtractor dataExtractor=new DataExtractor();
 
         //Act
+        boolean isFileRaid=dataExtractor.readFile("D:\\ElectionResult\\src\\test\\DataFilesTest\\demo.txt");
 
+        //Assert
         assertTrue(isFileRaid);
 
     }
+
+    @Test
+
+    void shouldAbleToTakeOnlyTextFileAsInput(){
+        //Arrange
+        DataExtractor dataExtractor=new DataExtractor();
+
+        // Act and Assert
+        assertThrows(IllegalFileFormatException.class, () -> {
+            dataExtractor.readFile("D:\\ElectionResult\\src\\test\\DataFilesTest\\demo.pdf");
+        });
+
+
+    }
+
 }
