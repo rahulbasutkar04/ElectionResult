078a3a9716952667062c85d402ae3f687950ae02
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-08 12:53:12 +0530

    Add Custom exception

---
+++ src/main/java/com/amaap/electionresult/service/exception/InvalidFilePathException.java
@@ -0,0 +1,7 @@
+package com.amaap.electionresult.service.exception;
+
+public class InvalidFilePathException extends Exception {
+    public InvalidFilePathException(String message) {
+        super(message);
+    }
+}
