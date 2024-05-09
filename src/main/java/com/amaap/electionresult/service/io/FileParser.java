8b1c8d966ce6b0a0ba9e20377c6fe0a53d9aad06
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-09 19:52:32 +0530

    Update in FileParser Class

--- src/main/java/com/amaap/electionresult/service/io/FileParser.java
+++ src/main/java/com/amaap/electionresult/service/io/FileParser.java
@@ -1,14 +1,18 @@
 package com.amaap.electionresult.service.io;
 
+import com.amaap.electionresult.service.exception.InvalidCityNameException;
+import com.amaap.electionresult.service.exception.InvalidFormatException;
+import com.amaap.electionresult.service.exception.InvalidPartyCodeException;
+
 public class FileParser {
 
     Validator validator = new Validator();
 
-    public boolean parser(String line) {
+    public boolean parser(String line) throws InvalidFormatException, InvalidCityNameException, InvalidPartyCodeException {
 
-        if (!(validator.isValidFormat(line))) return false;
-        if (!(validator.hasValidCityNames(line))) return false;
-
+        if (!(validator.isValidFormat(line))) throw new InvalidFormatException("InValid Format Of Line:"+line);
+        if (!(validator.hasValidCityNames(line))) throw  new InvalidCityNameException("Invalid City Name Found:"+line);
+        if(!(validator.containsOnlyValidPartyCodes(line))) throw new InvalidPartyCodeException("Invalid Party Code Found:"+line);
         return true;
     }
 
