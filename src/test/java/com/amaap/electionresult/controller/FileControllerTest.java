15788a2bb68fb84b0f3c5851ccbc19ddb7f46b0b
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-09 22:52:31 +0530

    Add test case in filecontroller

--- src/test/java/com/amaap/electionresult/controller/FileControllerTest.java
+++ src/test/java/com/amaap/electionresult/controller/FileControllerTest.java
@@ -4,14 +4,8 @@ import com.amaap.electionresult.controller.dto.Http;
 import com.amaap.electionresult.controller.dto.Response;
 import com.amaap.electionresult.repository.impl.InMemoryElectionRepositoryData;
 import com.amaap.electionresult.service.FileReaderService;
-import com.amaap.electionresult.service.exception.InvalidCityNameException;
-import com.amaap.electionresult.service.exception.InvalidFilePathException;
-import com.amaap.electionresult.service.exception.InvalidFormatException;
-import com.amaap.electionresult.service.exception.InvalidPartyCodeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,17 +16,31 @@ public class FileControllerTest {
 
     @BeforeEach
     void setup() {
-        inMemoryElectionRepositoryData=InMemoryElectionRepositoryData.getInstance();
+        inMemoryElectionRepositoryData = InMemoryElectionRepositoryData.getInstance();
         fileReaderService = new FileReaderService();
         inMemoryElectionRepositoryData.clear();
     }
 
     @Test
-    void shouldBeAbleToRespondWithOkMessageIfFileISTakenFromUser() throws InvalidFilePathException, IOException, InvalidFormatException, InvalidPartyCodeException, InvalidCityNameException {
+    void shouldBeAbleToRespondWithOkMessageIfFileISTakenFromUser() throws Response {
         // arrange
         FileController fileController = new FileController(fileReaderService);
         Response expected = new Response(Http.OK, Http.OK.getMessage());
         String path = "D:\\ElectionResult\\src\\main\\resources\\electiondata.text";
+
+        // act
+        Response actual = fileController.getFile(path);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldBeAbleToRespondWithBadRequestMessageIfGivenFilePathIsNotProceeded() throws Response {
+        // arrange
+        FileController fileController = new FileController(fileReaderService);
+        Response expected = new Response(Http.BAD_REQUEST, Http.BAD_REQUEST.getMessage());
+        String path = "D:\\ElectionResult\\src\\main\\resources\\invalidElectionData.txt";
 
         // act
         Response actual = fileController.getFile(path);
