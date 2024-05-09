78ecb07df140dc9e627d96bb0f786ccf4dc13cb9
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-09 19:54:57 +0530

    update in fileController Class

--- src/main/java/com/amaap/electionresult/controller/FileController.java
+++ src/main/java/com/amaap/electionresult/controller/FileController.java
@@ -3,7 +3,10 @@ package com.amaap.electionresult.controller;
 import com.amaap.electionresult.controller.dto.Http;
 import com.amaap.electionresult.controller.dto.Response;
 import com.amaap.electionresult.service.FileReaderService;
+import com.amaap.electionresult.service.exception.InvalidCityNameException;
 import com.amaap.electionresult.service.exception.InvalidFilePathException;
+import com.amaap.electionresult.service.exception.InvalidFormatException;
+import com.amaap.electionresult.service.exception.InvalidPartyCodeException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,7 +18,7 @@ public class FileController {
         this.fileReaderService = fileReaderService;
     }
 
-    public Response getFile(String path) throws InvalidFilePathException, IOException {
+    public Response getFile(String path) throws InvalidFilePathException, IOException, InvalidFormatException, InvalidPartyCodeException, InvalidCityNameException {
         if(fileReaderService.readFile(path)) return  new Response(Http.OK,Http.OK.getMessage());
 
         return new Response(Http.BAD_REQUEST,Http.BAD_REQUEST.getMessage());
