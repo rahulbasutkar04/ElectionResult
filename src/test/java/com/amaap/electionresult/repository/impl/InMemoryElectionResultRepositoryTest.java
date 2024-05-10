80f9d7db1f33b8c8c266ba3b757278afe898393e
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-10 12:49:49 +0530

    Add test case for repository

---
+++ src/test/java/com/amaap/electionresult/repository/impl/InMemoryElectionResultRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.amaap.electionresult.repository.impl;
+
+import com.amaap.electionresult.repository.electionResultRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryElectionResultRepositoryTest {
+    private electionResultRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = InMemoryElectionResultRepository.getInstance();
+        repository.clear();
+    }
+
+    @Test
+    public void shouldBeAbleToTestConstituencyResult() {
+        HashMap<String, HashMap<String, Integer>> testData = new HashMap<>();
+        HashMap<String, Integer> cityData = new HashMap<>();
+        cityData.put("BJP", 10000);
+        cityData.put("INC", 15000);
+        testData.put("Bangalore", cityData);
+
+        repository.ConstituencyResult(testData);
+
+        HashMap<String, HashMap<String, Integer>> resultData = repository.getWinnerData();
+        assertEquals(testData, resultData, "Inserted and retrieved data should match");
+    }
+
+    @Test
+    public void shouldBeAbleToTestGetWinnerData() {
+        HashMap<String, HashMap<String, Integer>> testData = new HashMap<>();
+        HashMap<String, Integer> cityData = new HashMap<>();
+        cityData.put("BJP", 10000);
+        cityData.put("INC", 15000);
+        testData.put("Bangalore", cityData);
+
+        repository.ConstituencyResult(testData);
+
+        HashMap<String, HashMap<String, Integer>> resultData = repository.getWinnerData();
+        assertEquals(testData, resultData, "Inserted and retrieved data should match");
+    }
+
+}
