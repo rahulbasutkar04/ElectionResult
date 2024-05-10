4caf0119364e4ce670ace7e1f0379b33c4313633
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-10 12:44:58 +0530

    Add data extractor class

---
+++ src/main/java/com/amaap/electionresult/domain/service/DataExtractor.java
@@ -0,0 +1,27 @@
+package com.amaap.electionresult.domain.service;
+
+import java.util.HashMap;
+
+public class DataExtractor {
+
+    public  HashMap<String, HashMap<String, Integer>> ExtractData(String input) {
+        HashMap<String, HashMap<String, Integer>> resultMap = new HashMap<>();
+
+        String[] cities = input.split(";");
+        for (String cityData : cities) {
+            String[] parts = cityData.split(", ");
+            String city = parts[0];
+            HashMap<String, Integer> partyMap = new HashMap<>();
+
+            for (int i = 1; i < parts.length - 1; i += 2) {
+                String party = parts[i + 1];
+                int votes = Integer.parseInt(parts[i]);
+                partyMap.put(party, votes);
+            }
+
+            resultMap.put(city, partyMap);
+        }
+
+        return resultMap;
+    }
+}
